770cf1b70b230e3b6820cd4f76d55b97b2940ba3
xsummersx <605369747@qq.com>
2020-06-28 19:47:18 +0800

    commonents

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,24 @@
 <template>
-  <div></div>
+  <div>
+    <footer class="footer">
+      <div class="footer__wrap">
+        <div class="footer__secondary">
+          <div class="footer__inner">
+            <div class="footer__secondary__nav">
+              <span><a target="_blank">河畔一角主页</a>|<a target="_blank">Vue全栈打造商城</a>|<a
+                  target="_blank">React全家桶打造通用后台</a>|<a target="_blank">微信支付专项(H5/小程序/小程序云+Node+Mongo)</a></span>
+              <p>
+                <a>关于我们</a>|
+                <a href="#">条款和条件</a>|
+                <a href="#">隐私政策</a>
+              </p>
+              <p>Copyright © 2019 河畔一角 版权所有</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import Router from './router'
+import './assets/css/base.css'
+import './assets/css/index.css'
 Vue.config.productionTip = false //提示开关
 Vue.use(VueAxios, axios)
 new Vue({
