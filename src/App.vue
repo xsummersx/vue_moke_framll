97bd8cdbf5ca83b1a5d99a461d3be8604147e4bb
xsummersx <605369747@qq.com>
2020-06-28 16:33:46 +0800

    this 8 ge gouzi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -12,6 +12,30 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  beforeCreate () {
+
+  },
+  created () {
+
+  },
+  beforeMount () {
+
+  },
+  mounted () {
+
+  },
+  beforeUpdate () {
+
+  },
+  updated () {
+
+  },
+  beforeDestroy () {
+
+  },
+  destroyed () {
+
   }
 }
 </script>
