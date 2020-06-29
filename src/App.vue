fe75d0213b9b2606a35c684826910bcaf347b92c
xsummersx <605369747@qq.com>
2020-06-29 10:28:46 +0800

    router updata

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <router-link class="box" to='/'>cart</router-link>
+    <router-link class="box" to='/address'>address</router-link>
     <router-view></router-view>
   </div>
 </template>
